2648436c81c2e4fbd2211420fe52684b83c4ecce
Aryaiqbal79 <136413219+Aryaiqbal79@users.noreply.github.com>
2024-02-05 11:46:27 +0700

    login.js

---
+++ login.js
@@ -0,0 +1,5 @@
+function login() {
+    // Perform login logic (you may need to check credentials and redirect to home.html)
+    // For simplicity, let's just redirect to home.html
+    window.location.href = "home.html";
+}
